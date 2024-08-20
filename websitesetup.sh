d96112d1a734b9116039103338ee22c2dc78a274
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2024-08-20 22:29:53 +0530

    variables in shell scripting

--- websitesetup.sh
+++ websitesetup.sh
@@ -1,43 +1,51 @@
 #!/bin/bash
 
+# declare variables
+PACKAGE="wget unzip apache2"
+SVC="apache2"
+TMP_DIR="/tmp/webfiles"
+URL="https://www.tooplate.com/zip-templates/2135_mini_finance.zip"
+TEMPLATE="2135_mini_finance"
+
 # install dependencies
 echo "===================Installing Dependecies========================="
-sudo apt install wget unzip apache2 -y > /dev/null
+sudo apt install $PACKAGE -y > /dev/null
 
 echo""
 
 # enable httpd
 echo "======enabling apache2=========="
-sudo systemctl start apache2
-sudo systemctl enable apache2
+sudo systemctl start $SVC
+sudo systemctl enable $SVC
 
 echo ""
 
 # creating folder 
 echo "------creating folder /tmp/webfiles---------"
-mkdir -p /tmp/webfiles
-cd /tmp/webfiles
+mkdir -p $TMP_DIR
+cd $TMP_DIR
 
 echo ""
 
 echo "=======Downloading template========"
-wget https://www.tooplate.com/zip-templates/2135_mini_finance.zip > /dev/null
+wget $URL > /dev/null
 
 echo ""
 
 echo "=========Copying template to /var/www/html============"
-unzip 2135_mini_finance.zip > /dev/null
-sudo cp -r 2135_mini_finance/* /var/www/html
+unzip $TEMPLATE.zip > /dev/null
+sudo cp -r $TEMPLATE/* /var/www/html
 
 echo ""
 
-rm -rf /tmp/webfiles
+rm -rf $TMP_DIR
 
 echo "============Restarting apache2============="
-sudo systemctl restart apache2
+sudo systemctl restart $SVC
 
-rm -rf /tmp/webfiles 
+rm -rf $TMP_DIR
 
-sudo systemctl status apache2
+
+sudo systemctl status $SVC
 
 ls /var/www/html
